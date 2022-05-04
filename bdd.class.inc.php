b6dbd4745507d1ac7a29c92c5dbf32465d2ae882
GeraudBeshenT <geraudbesson@gmail.com>
2022-05-04 21:24:20 +0200

    .

--- bdd.class.inc.php
+++ bdd.class.inc.php
@@ -2,7 +2,7 @@
 // connection à la base de donnée shinystock.
 try 
 {
-    $conn = new PDO("mysql:host=127.0.0.1;dbname=dd", "root", "");
+    $conn = new PDO("mysql:host=127.0.0.1;dbname=uvlight", "root", "");
     $conn->exec("SET CHARACTER SET utf8");
 } 
 catch (PDOException $except) 
